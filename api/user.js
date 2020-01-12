c175516bc192e5d0a4a6a5c076fff7a64615c866
Dillon <18711345569@163.com>
2020-01-12 20:57:16 +0800

    finish the function of registering

---
+++ api/user.js
@@ -0,0 +1,43 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  return request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
+
+export function sendSms(mobile) {
+  return request({
+    url: `/user/user/sendsms/+${mobile}`,
+    method: 'get'
+  })
+}
+
+export function register(user, code) {
+  return request({
+    url: `/user/user/register/+${code}`,
+    method: 'post',
+    data: user
+  })
+}
+
