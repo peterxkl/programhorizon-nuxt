629254ffb807edb8d473abe11a84dd3e0d7450cf
Dillon <18711345569@163.com>
2020-01-12 14:57:29 +0800

    finish the share function

--- pages/gathering/item/_id.vue
+++ pages/gathering/item/_id.vue
@@ -1,23 +1,89 @@
 <template>
-    <div>
-      活动详情
-      {{item.id}}
-      <hr>
-      {{item.name}}
+  <div class="wrapper activities">
+    <h1>{{item.id}}</h1>
+    <div class="img-text">
+      <div class="left-img">
+        <img src="~/assets/img/widget-acti.png" alt="" />
+      </div>
+      <div class="right-txt">
+        <p>开始时间： {{item.startTime}}</p>
+        <p>结束时间： {{item.endTime}}</p>
+        <p>举办地点： {{item.address}}</p>
+        <p>主办方：infoQ</p>
+        <p>报名截止： <span class="big">85</span>天 <span class="big">15</span>时 <span class="big">16</span>分 <span class="big">10</span>秒</p>
+        <div class="join">
+          <button class="sui-btn btn-danger">立即报名</button>
+          <span class="will">报名即将开始</span>
+        </div>
+      </div>
     </div>
+    <div class="simple-text">
+      <div class="left-content">
+        <div class="content-item">
+          <div class="tit">
+            <span>大会介绍</span>
+          </div>
+          <div class="text">
+            <h4></h4>
+            <p>{{item.summary}}</p>
+          </div>
+        </div>
+        <div class="content-item">
+          <div class="tit">
+            <span>议题简介</span>
+          </div>
+          <div class="text">
+            <h4></h4>
+            <p>{{item.detail}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="right-intro">
+        <div class="content-item">
+          <div class="tit">
+            <span>活动组织者</span>
+          </div>
+          <div class="text">
+            <p>主办方： {{item.sponsor}}</p>
+          </div>
+        </div>
+        <div class="content-item">
+          <div class="tit">
+            <span>相关链接</span>
+          </div>
+          <div class="text">
+            <p>活动官网： infoQ.com</p>
+          </div>
+        </div>
+        <div class="content-item">
+          <div class="tit">
+            <span>分享扩散</span>
+          </div>
+          <div class="social-share" data-sites="weibo,wechat" data-url="http://baidu.com" :data-title="item.name">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
-    export default {
-        asyncData({params}) {
-            return axios.get(`http://localhost:3000/mock/11/gathering/${params.id}`).then(
-                res => {
-                    return {item: res.data.data}
-                }
-            )
-        }
+  import gatheringApi from '@/api/gathering'
+  export default {
+    asyncData({params}) {
+      return gatheringApi.findById(params.id).then(res => {
+        return {item: res.data.data}
+      })
+    },
+    head: {
+      scrtpt: [
+        {src: 'https://cdn.bootcss.com/social-share.js/1.0.16/js/social-share.min.js'}
+      ],
+      link: [
+        {rel: 'stylesheet',href: 'https://cdn.bootcss.com/social-share.js/1.0.16/css/share.min.js'}
+      ]
     }
+  }
 </script>
 
 <style scoped>
